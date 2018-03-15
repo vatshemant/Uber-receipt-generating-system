12d2d55f8cc1eba5c6f8b5c3299c1715110c2fbf
Hemant Vats <hemant02.vats@gmail.com>
2018-03-15 23:28:32 +0530

    Main method of Program

--- MainMethod.java
+++ MainMethod.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainMethod {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		SelectCategory option = new SelectCategory();
+		SelectCategory option = new SelectCategory();//creating object.
 		option.userCategory();
 		int c = sc.nextInt();
 		option.choseCategory(c);
