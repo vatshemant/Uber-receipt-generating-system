6c06c6aac1bdeca5273d37b9c3454a38937a6e60
Hemant Vats <hemant02.vats@gmail.com>
2018-03-22 22:34:54 +0530

    Update OptionPassenger.java

--- OptionPassenger.java
+++ OptionPassenger.java
@@ -81,7 +81,7 @@ public class OptionPassenger {
 		}
 
 	}
-	public void passengerList(OptionPassenger passenger) {
+	public void passengerList(OptionPassenger passenger) { //Function to store passenger details after each ride.
 		ArrayList passengerlist = new ArrayList();
 		passengerlist.add("Passenger Destination");
 		passengerlist.add(passenger.destinationDetails);
