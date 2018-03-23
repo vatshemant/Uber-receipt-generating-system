7ed3055ebccd5009442b305545868af857c2f4a6
Hemant Vats <hemant02.vats@gmail.com>
2018-03-23 23:31:42 +0530

    Class Main Method

--- MainMethod.java
+++ MainMethod.java
@@ -1,22 +1,24 @@
 package uberReciptSystem;
-
 import java.util.Scanner;
 
 public class MainMethod {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		SelectCategory option = new SelectCategory();//Select Category Class Object.
-		OptionPassenger passenger = new OptionPassenger();//Passenger Class Object.
-		OptionDriver driver = new OptionDriver();//Driver Class Object.
-		OptionCorporate corporate = new OptionCorporate();//Corporate Class Object.
-		while (!false) {
+		SelectCategory option = new SelectCategory();
+		OptionPassenger passenger = new OptionPassenger();
+		OptionDriver driver = new OptionDriver();
+		OptionCorporate corporate = new OptionCorporate();
+		 while(true){
 			option.userCategory();
 			int c = sc.nextInt();
-			if (c == 4) {
+			
+			option.choseCategory(c, driver, passenger, corporate);
+			System.out.println(":::DO YOU WANT TO QUIT (Y/N) :::");
+			char ch;
+			ch=sc.next().charAt(0);
+			if(ch=='n'||ch=='N') {
 				break;
 			}
-			option.userCategory();
-			option.choseCategory(c, driver, passenger, corporate);//Passing objects in Chose Category Function.
 		}
 	}
 }
