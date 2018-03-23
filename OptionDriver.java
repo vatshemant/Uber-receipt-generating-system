07657ab80117d960de1ba8d01bac8cfb97a46dfa
Hemant Vats <hemant02.vats@gmail.com>
2018-03-23 23:36:20 +0530

    Driver Class

--- OptionDriver.java
+++ OptionDriver.java
@@ -8,16 +8,16 @@ public class OptionDriver {
 	String name;
 	int vehicleNumber;
 
-	void inputDetails() {
+	void inputDetails() { //Input Details function to take Details of Driver.
 		System.out.println("::::ENTER YOUR DETAILS::::");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("ENTER YOUR NAME");
 		name = sc.nextLine();
 		System.out.println("ENTER VEHICLE NUMBER");
 		vehicleNumber = sc.nextInt();
-	}
+	/*}
 
-	/*void displayDetails(OptionPassenger passenger) {
+	void displayDetails(OptionPassenger passenger) {
 		System.out.println("DRIVER NAME::::::::: " + name);
 		System.out.println("VEHICLE NUMBER:::::: " + vehicleNumber);
 		System.out.println("DISTANCE TRAVELLED:: " + passenger.distance + " kms.");
