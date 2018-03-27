d279b7c77401d98583def3f647f62cede2ca0076
Hemant Vats <hemant02.vats@gmail.com>
2018-03-27 23:08:24 +0530

    DB Connection Class

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,25 +3,45 @@ package uberReciptSystem;
 import java.sql.DriverManager;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
-public class DatabaseConnection {
+public class DbTableCreate {
+	private static final Statement NULL = null;
 
-	public static Connection getConnection() throws Exception{
-		 try{
-			   String driver = "com.mysql.jdbc.Driver";
-			   String url = "jdbc:mysql://localhost:3306/uber";
-			   String username = "root";
-			   String password = "root";
-			   Class.forName(driver);
-			   
-			   Connection conn = (Connection) DriverManager.getConnection(url,username,password);
-			   System.out.println("Connected");
-			   return conn;
-			  } catch(Exception e){System.out.println(e);}
-			  
-		  return null;
+	public static Connection getConnection() throws Exception {
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/uber";
+			String username = "root";
+			String password = "root";
+			Class.forName(driver);
+
+			Connection conn = (Connection) DriverManager.getConnection(url, username, password);
+			Statement stmt=NULL;
+			System.out.println("Connected");
+			System.out.println("Creating table in given database...");
+		      stmt = (Statement) conn.createStatement();
+		      
+		      String sql = "CREATE TABLE UberDetails" +
+		                   "(DriverName VARCHAR(255), " +
+		                   " VehicleNumber INTEGER, " + 
+		                   " DistanceCovered INTEGER, " + 
+		                   " AmountEarned INTEGER, " + 
+		                   " DriverAlloted INTEGER,"+
+		                   "PassengerDestination VARCHAR(255),"+
+		                   "RideChosen VARCHAR(255),"+
+		                   "DistanceTravelled INTEGER,"+
+		                   "AmountPaid INTEGER)       "; 
+		      stmt.executeUpdate(sql);
+		     
+			return conn;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return null;
 	}
-	public static void main(String args[]) throws Exception {
-		getConnection();
-	}
+	
 }
+
+
