3e113656462800ce177593af04244bc3fa87d0ec
Hemant Vats <hemant02.vats@gmail.com>
2018-04-04 23:09:07 +0530

    Main Method

--- MainMethod.java
+++ MainMethod.java
@@ -3,16 +3,19 @@ package uberReciptSystem;
 import java.util.Scanner;
 
 public class MainMethod {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		SelectCategory option = new SelectCategory();//Category Class Object.
+		SelectCategory option = new SelectCategory();
 		OptionPassenger passenger = new OptionPassenger();
-		OptionDriver driver = new OptionDriver();//Driver Class Object.
-		OptionCorporate corporate = new OptionCorporate();//Corporate Class Object.
+		OptionDriver driver = new OptionDriver();
+		OptionCorporate corporate = new OptionCorporate();
+		DbConnect connect1=new DbConnect();
 		while (true) {
 			option.userCategory();
 			int c = sc.nextInt();
 			option.choseCategory(c, driver, passenger, corporate);
+			connect1.UpdateDriverData(driver,passenger);
+			connect1.UpdatePassengerData(passenger);
 			System.out.println(":::DO YOU WANT TO QUIT (Y/N) :::");
 			char ch;
 			ch = sc.next().charAt(0);
