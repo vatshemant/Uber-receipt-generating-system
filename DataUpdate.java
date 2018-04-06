882b7f4c63d22027a2f0a6988c0170af8abc3ddd
Hemant Vats <hemant02.vats@gmail.com>
2018-04-07 00:22:56 +0530

    Update DataUpdate.java

--- DataUpdate.java
+++ DataUpdate.java
@@ -9,7 +9,7 @@ public class DbConnect {
 	static Statement stmt=NULL;
 	
 
-	public void UpdateDriverData(OptionDriver driver,OptionPassenger passenger) throws Exception{
+	public void UpdateDriverData(OptionDriver driver,OptionPassenger passenger) throws Exception{//Updating data in driver table
 		String driver1 = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/uber";
 		String username = "root";
@@ -21,7 +21,7 @@ public class DbConnect {
 	      stmt.executeUpdate("INSERT INTO UberDriver (DriverName,VehicleNumber,DistanceCovered,AmountEarned) VALUES ('"+driver.name+"','"+driver.vehicleNumber+"','"+passenger.distance+"','"+passenger.fare+"')");
 	     
 	}
-	public void UpdatePassengerData(OptionPassenger passenger)throws Exception {
+	public void UpdatePassengerData(OptionPassenger passenger)throws Exception {//Updating Data in passenger tablr
 		String driver1 = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/uber";
 		String username = "root";
