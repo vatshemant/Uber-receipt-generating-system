cc6d2fe8eea55bbc246a0e6c8112107637cec709
Hemant Vats <hemant02.vats@gmail.com>
2018-04-26 00:20:18 +0530

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class MainMethod {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		SelectCategory option = new SelectCategory();//Category class object.
-		OptionPassenger passenger = new OptionPassenger();//Passenger class object
-		OptionDriver driver = new OptionDriver();//Driver class object.
-		OptionCorporate corporate = new OptionCorporate();//Corporate class object.
+		SelectCategory option = new SelectCategory();
+		OptionPassenger passenger = new OptionPassenger();
+		OptionDriver driver = new OptionDriver();
+		OptionCorporate corporate = new OptionCorporate();
 		DbConnect connect1=new DbConnect();
 		while (true) {
 			option.userCategory();
