7d2f9002c0e284b0330df8cb8836077679c19e83
Hemant Vats <hemant02.vats@gmail.com>
2018-04-27 23:17:15 +0530

    Database Connection Class

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -16,7 +16,7 @@ public class DbTableCreate {
 			String password = " ";
 			Class.forName(driver);
 
-			Connection conn = (Connection) DriverManager.getConnection(url, username, password);
+			Connection conn = (Connection) DriverManager.getConnection(url, username, password);//Establishing Connection to the server.
 			Statement stmt=NULL;
 			System.out.println("Connected");
 			System.out.println("Creating table in given database...");
